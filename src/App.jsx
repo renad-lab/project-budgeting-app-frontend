bf003617fd2ff12264a1ade74281b81a00bed0bb
renad-lab <renad@pursuit.org>
2024-07-03 14:04:05 -0400

    Updated codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+// import "./App.css";
+// import NavBar from "./Components/NavBar/NavBar";
+// import Show from "./Components/Show/Show";
+// import New from "./Components/New/New";
+// import Edit from "./Components/Edit/Edit";
+// import Home from "./Components/Home/Home";
+// import { Routes, Route, Navigate } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <div>
+//       <NavBar />
+//       <Routes>
+//         <Route path="/" element={<Navigate to="/transactions" replace />} />
+//         <Route path="/transactions" element={<Home />} />
+//         <Route path="/transactions/new" element={<New />} />
+//         <Route path="/transactions/:index" element={<Show />} />
+//         <Route path="/transactions/:index/edit" element={<Edit />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
-// import Home from "./Components/Home";
 import Show from "./Components/Show/Show";
 import New from "./Components/New/New";
 import Edit from "./Components/Edit/Edit";
